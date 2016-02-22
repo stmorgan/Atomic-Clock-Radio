4bdef7c61dbda44aa724375847b43704c243bd01
Scott Morgan <scott@scottmorgan.ca>
2016-02-21 18:07:16 -0800

    Moved printing radio station on LCD to procedure.

--- AtomicClockRadio.ino
+++ AtomicClockRadio.ino
@@ -114,21 +114,19 @@ float flt_channel;
   
   serviceRadio();
   serviceGPS();
-     
-  // Print radio channel 
+  displayRadioInfo();                          // Print radio channel 
+ }// End of Main Loop
 
- //  str_channel = String(channel);
-
-  lcd.setCursor(11,0);  
+void displayRadioInfo() {
+    lcd.setCursor(11,0);  
   if (channel < 1000) {
     lcd.print(F(" "));
     // lcd.setCursor(12,0);
   }
   lcd.print(channel / 10);                   // Print integer part of FM frequency.
   lcd.print(F("."));                         // Print decimal point.
-  lcd.print(channel - 10 * (channel / 10));  // Print decimal digit in FM frequency.
- // delay(1000); // Wait one second.
-}// End of Main Loop
+  lcd.print(channel - 10 * (channel / 10));  // Print decimal digit in FM frequency
+}// End of displayRadioInfo
 
 void displayInfo()
 {
